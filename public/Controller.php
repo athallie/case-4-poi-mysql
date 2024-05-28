ae8a8dc80ac811bbdf283f441504a5152ccd2fb3
icy <128914324+0xicy@users.noreply.github.com>
2024-05-28 11:30:04 +0700

    Update Controller.php

--- public/Controller.php
+++ public/Controller.php
@@ -14,4 +14,17 @@ class Controller
         $status = $model->create($latitude, $longitude, $name, $description);
         echo $status ? "Data Added" : "Failed to Add Data";
     }
+    
+    function update($data)
+    {
+        $latitude = $data["latitude"];
+        $longitude = $data["longitude"];
+        $name = $data["name"];
+        $description = $data["description"];
+
+        require_once('Model.php');
+        $model = new Model("localhost", "root", "uxn265zc14", "case_4", 33060);
+        $status = $model->update($latitude, $longitude, $name, $description);
+        echo $status ? "Data Updated" : "Failed to Update Data";
+    }
 }
